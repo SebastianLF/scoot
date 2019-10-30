024655b963e5da10e0d44dd61be073d0bb29b059
SebastianLF <slf.pro@gmail.com>
2019-10-30 05:50:15 +0100

    implement home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react"
+import Select from "react-select"
+import styled from "styled-components"
+import ProjectLine from "../../components/ProjectLine"
+import api from '../../utils/api'
+import { languages, frameworks } from '../../utils/data'
+
+const Container = styled.div`
+  font-size: 1rem;
+  color: grey;
+`
+const Projects = styled.div``
+
+
+export default function Home() {
+  const [projects, setProjects] = useState([])
+  const [isError, setIsError] = useState(false)
+  const [search, setSearch] = useState([])
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const fetchedProjects = await api.projects.search(search)
+        console.log(fetchedProjects)
+
+        setProjects(fetchedProjects)
+
+      } catch (error) {
+        console.log(error);
+
+        setIsError(true)
+      }
+    }
+
+    fetchProjects()
+  }, [search])
+
+  const filterSearch = (e) => {
+    console.log(e)
+
+    setSearch(e)
+  }
+
+  return (
+    <Container>
+      <Select
+        options={languages}
+        placeholder="Search..."
+        onChange={filterSearch}
+        isMulti
+      />
+      {isError ? (
+        <div>Something went wrong</div>
+      ) : (
+          <Projects>
+            {projects.map(project => <ProjectLine key={project.id} {...project} />)}
+          </Projects>
+        )
+      }
+
+    </Container>
+  )
+}
