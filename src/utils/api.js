37dd659624639298dbe75e5140bba420a3467625
SebastianLF <slf.pro@gmail.com>
2019-11-03 14:54:26 +0100

    implement filtering projects

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,63 +1,77 @@
 import { languages, frameworks } from './data'
-import selection from '../assets/selection.json'
 import faker from 'faker'
 import { randomNumber, randomPicks } from './random'
+import uuid from 'uuid'
 
-// const options = initOptions(selection)
-
-// function initOptions(selection) {
-//     return selection.icons.map((skill) => skill.properties && skill.properties.name ? ({
-//         value: skill.properties.name,
-//         label: skill.properties.name
-//     }) : '')
-// }
-
-const projects = {
-    getAll: () => generateProjects(),
+const data = {
+    projects: [],
+    options: []
 }
 
-const generateProjects = () => new Promise((resolve, reject) => {
+// Generate fake random projects
+const generateProjects = () => {
     let projects = []
 
-    try {
-        for (let i = 0; i < randomNumber(20); i++) {
-            const newProject = {
-                posted: 'Today',
-                title: faker.name.title(),
-                description: faker.lorem.paragraphs(),
-                languages: randomPicks(languages, randomNumber(2)),
-                skills: randomPicks(frameworks, randomNumber(8)),
-                difficulty: randomPicks(['Entry', 'Intermediate', 'Experienced']),
-                timezone: randomPicks(['UTC', 'UTC+2', 'UTC+8']),
-                participants: randomPicks([{
-                    name: faker.internet.userName(),
-                    email: faker.internet.email()
-                },
-                {
-                    name: faker.internet.userName(),
-                    email: faker.internet.email()
-                },
-                {
-                    name: faker.internet.userName(),
-                    email: faker.internet.email()
-                },
-                {
-                    name: faker.internet.userName(),
-                    email: faker.internet.email()
-                }], randomNumber(4))
-            }
-
-            projects.push(newProject)
+    for (let i = 0; i < randomNumber(20); i++) {
+        const newProject = {
+            id: uuid(),
+            posted: 'Today',
+            title: faker.name.title(),
+            description: faker.lorem.paragraphs(),
+            languages: randomPicks(languages, randomNumber(2)),
+            skills: randomPicks(frameworks, randomNumber(8)),
+            difficulty: randomPicks(['Entry', 'Intermediate', 'Experienced']),
+            timezone: randomPicks(['UTC', 'UTC+2', 'UTC+8']),
+            participants: randomPicks([{
+                name: faker.internet.userName(),
+                email: faker.internet.email()
+            },
+            {
+                name: faker.internet.userName(),
+                email: faker.internet.email()
+            },
+            {
+                name: faker.internet.userName(),
+                email: faker.internet.email()
+            },
+            {
+                name: faker.internet.userName(),
+                email: faker.internet.email()
+            }], randomNumber(4))
         }
-        return resolve(projects)
-    } catch (error) {
-        return reject('Error!')
-    }
-})
 
+        projects.push(newProject)
+    }
+    return projects
+}
+data.projects = generateProjects()
+
+// faking server side projects filtering.
+const filter = (filters) => {
+
+    // return all projects when filters equals 'null'.
+    if (!filters) return data.projects
+
+    return data.projects.filter((project) => {
+        const languagesAndSkills = [...project.languages, ...project.skills]
+
+        return filters.every((element) => {
+            const foundElement = languagesAndSkills.some(skill => skill.id === element.id)
+            if (foundElement) return true
+
+            return false
+        })
+    })
+}
+
+// API
+const APIprojects = {
+    search: (filters) => new Promise((resolve, reject) => resolve(filter(filters)))
+}
+
+data.options = [...languages, ...frameworks]
 
 export default {
-    options: [...languages, ...frameworks],
-    projects,
-    generateProjects
+    options: data.options,
+    projects: APIprojects
 }
