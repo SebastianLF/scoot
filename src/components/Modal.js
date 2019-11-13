ce135dd4676155b28b6ca8e6508d0ee3739eb2e8
SebastianLF <slf.pro@gmail.com>
2019-11-13 13:35:15 +0100

    add compelling inputs inside Modal for new proj

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+const Title = styled.input``
+const Languages = styled.select``
+const Tools = styled.select``
+const Level = styled.select``
+const Timezone = styled.select``
+
 export default function Modal() {
     return (
         <div>
-            <Form></Form>
+            <Form>
+                <Title></Title>
+                <Languages></Languages>
+                <Tools></Tools>
+                <Level></Level>
+                <Timezone></Timezone>
+            </Form>
         </div>
     )
 }
