816e58125e0cdf8af189e4c7e671f5951456ab6c
SebastianLF <slf.pro@gmail.com>
2019-11-13 13:30:00 +0100

    implement 404 component

---
+++ src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoMatch() {
+    return (
+        <div>
+            <p>This link is broken or this page has been moved.</p>
+        </div>
+    )
+}
